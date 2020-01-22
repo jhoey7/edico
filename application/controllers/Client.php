7e1656a1ef5c459f11858f6461aae91943dd7a4c
Joni Iskandar <jhoey.potter7@gmail.com>
2020-01-22 10:15:55 +0700

    Penambahan Client

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -20,7 +20,7 @@ class Client extends CI_Controller {
 	
 	private function configisg() {
 		$apppath =  str_replace("\\", "/", BASEPATH);
-		$fileconfig = $apppath.'client.txt';
+		$fileconfig = $apppath.'client.txt';echo $fileconfig;die();
 		$file = file_get_contents($fileconfig, true);
 
 		$lines = explode("\n", $file);
@@ -60,88 +60,12 @@ class Client extends CI_Controller {
 				$sites = trim($sites);
 				$pos = strpos($sites, 'wp-content');
 				$newStr = substr($sites,0,$pos );
-				echo $newStr . " </ br>";
 			}
 		}
 	}
 
-	public function execute() {
-		$this->load->library('PHPExcel');
-		$error = "";
-
-		$fileList = glob('/home/HITACHI/ftpfolder/*.xls');
-		foreach ($fileList as $filename) {
-			if (is_file($filename)) {
-				$file = basename($filename, ".xls");
-				$arrfile = explode("_", $file);
-
-				if (strtolower($arrfile[0]) == "laporanpemasukanbahanbaku") {
-					PHPExcel_Settings::setZipClass(PHPExcel_Settings::PCLZIP);
-					$objPHPExcel = PHPExcel_IOFactory::load($file);
-					$objPHPExcel->setActiveSheetIndex(0);
-
-					foreach($objPHPExcel->getWorksheetIterator() as $worksheet){
-						$highestRow         = $worksheet->getHighestRow(); 
-
-						$highestColumn      = $worksheet->getHighestColumn(); 
-						$highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-						$index = 0;
-						for ($row=2; $row <= $highestRow; $row++) {
-							for ($col=0; $col <= ($highestColumnIndex-1); $col++) {
-								$cell = $worksheet->getCellByColumnAndRow($col, $row);
-								$CELLDATA = $cell->getCalculatedValue();
-								$DATAEXCEL[$index][$field[$col]] = str_replace("'","",$CELLDATA);
-							}
-							$index++;
-						}
-
-						foreach ($DATAEXCEL as $VAL) {
-							if (count($arr_barang_validasi[$VAL['KODE_BARANG']]) == 0) {
-								$barang['kd_brg'] = $VAL['KODE_BARANG'];
-								$barang['jns_brg'] = $VAL['JENIS_BARANG'];
-								$barang['nm_brg'] = $VAL['URAIAN_BARANG'];
-								$barang['nilai_konversi'] = $VAL['NILAI_KONVERSI'];
-								$barang['kd_satuan'] = $VAL['KODE_SATUAN'];
-								$barang['kd_satuan_terkecil'] = $VAL['KODE_SATUAN_TERKECIL'];
-								$barang['kode_trader'] = $KODE_TRADER;
-								
-								#insert ke master barang
-								$this->db->insert('tm_barang', $barang);
-								$id_barang = $this->db->insert_id();
-
-								#insert ke master barang gudnag
-								$this->db->insert('tm_barang_gudang', array(
-									"id_barang"=> $id_barang,
-									"id_gudang"=> $arr_gudang_validasi[$VAL['KODE_GUDANG']]['id']
-								));
-								$id_gudang = $this->db->insert_id();
-
-								$arr_barang_validasi[$VAL['KODE_BARANG']]['id'] = $id_barang;
-								$arr_barang_validasi[$VAL['KODE_BARANG']]['kd_brg'] = $VAL['KODE_BARANG'];
-								$arr_barang_validasi[$VAL['KODE_BARANG']]['jns_brg'] = $VAL['JENIS_BARANG'];
-								$berhasil += 1;
-							} else {
-								$id_barang = $arr_barang_validasi[$VAL['KODE_BARANG']]['id'];
-
-								$sql = "SELECT a.id FROM tm_barang_gudang a 
-										LEFT JOIN tm_barang b ON b.id = a.id_barang 
-										LEFT JOIN tm_warehouse c on c.id = a.id_gudang
-										WHERE a.id_barang = ".$this->db->escape($id_barang)." 
-										AND c.kode = ".$this->db->escape($VAL['KODE_GUDANG']);
-								$hasil = $this->db->query($sql);
-								if ($hasil->num_rows() == 0) {
-									$this->db->insert('tm_barang_gudang', array(
-										"id_barang"=> $id_barang,
-										"id_gudang"=> $arr_gudang_validasi[$VAL['KODE_GUDANG']]['id']
-									));
-									$berhasil += 1;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+	public function execute($tipe) {
+		
 	}
 
 }
